f96156c13d28b567e85ef47df8492bd74c6c24e8
shanthi <shanbojja@gmail.com>
2026-08-28 23:26:14 +0530

    Terraform

--- state/provider.tf
+++ state/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84ks-remote-state"
+    bucket = "ks-s3-testing-remote-state"
     key    = "remote-state-demo"
     region = "us-east-1"
     encrypt        = true
