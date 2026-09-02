f4393f12b1b8adc20bfe8e9e87aa24cae08c3289
shanthi <shanbojja@gmail.com>
2026-09-02 17:09:49 +0530

    Terraform

--- secure-state/provider.tf
+++ secure-state/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "ks-secure-state-test"
+    bucket = "ks-s3-testing-remote-state"
     key    = "secure"
     region = "us-east-1"
     encrypt        = true
